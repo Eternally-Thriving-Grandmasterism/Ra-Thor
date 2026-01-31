f6cb3a0fd66c9b5fd94f53703df046268df236cc
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-31 18:41:15 -0500

    Create HeatShield.jsx

---
+++ core/components/HeatShield.jsx
@@ -0,0 +1,14 @@
+export default function HeatShield() {
+  return (
+    <div style={{
+      fontSize: '5em',
+      color: '#ff4444',
+      opacity: 0.3,
+      textAlign: 'center',
+      margin: '2rem 0',
+      userSelect: 'none'
+    }}>
+      🛡️ Already whole
+    </div>
+  );
+}
