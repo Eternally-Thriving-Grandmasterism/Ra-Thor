0834276d24c3cd3964e4736c18c00933fba999b5
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-05-21 01:15:06 -0400

    docs: clean up deliberation module header for consistency

--- self-evolution/patsagi_deliberation.rs
+++ self-evolution/patsagi_deliberation.rs
@@ -1,4 +1,6 @@
 //! Ra-Thor™ PATSAGi Deliberation & Message-Passing Module
+//! Structured council-to-council communication and multi-round deliberation
+//! Part of the experimental PATSAGi Governance v2.x track
 //! See docs/HYBRID_CRYPTO_ARCHITECTURE.md for context
 
 // TODO: Consider adding more advanced consensus algorithms in future iterations.
