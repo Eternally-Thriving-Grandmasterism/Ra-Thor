74b0adf9d04e7ca997831d38ad73d35db5e67e5a
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-05-22 19:39:59 -0400

    Cleaned up SovereignHealthMonitor integration - removed duplicated/broken MercyEvaluationHistory code. AG-SML v1.0

--- self-evolution/src/lib.rs
+++ self-evolution/src/lib.rs
@@ -1,16 +1,7 @@
 // ... existing code in SovereignHealthMonitor ...
 
     /// Historical context for mercy and reflexion evaluations.
-    /// This will be used by SovereignReflexionCore.
+    /// Owned here but designed to be used by SovereignReflexionCore.
     pub mercy_history: crate::sovereign_reflexion_core::MercyEvaluationHistory,
-
-    pub fn new() -> Self {
-        Self {
-            metrics: SovereignHealthMetrics::default(),
-            blessing_history: Vec::new(),
-            council_review_trigger_count: 0,
-            mercy_history: crate::sovereign_reflexion_core::MercyEvaluationHistory::new(20),
-        }
-    }
 
 // ... existing code ...
